b60c0c06eecadf77ce1aac993ebbb22713a53ff5
Manesh <manisht@fin-techsolutions.com>
2023-04-27 11:43:57 +0530

    first commit

---
+++ blogger-core/src/main/java/com/blogger/core/Main.java
@@ -0,0 +1,10 @@
+package com.blogger.core;
+
+import com.blogger.util.Utility;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Utility.isNull(null));
+    }
+}
