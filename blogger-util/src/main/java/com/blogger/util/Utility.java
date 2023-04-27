620029f8de8ad5565aca06f257ee8c9f8c40ac4d
Manesh <manisht@fin-techsolutions.com>
2023-04-27 13:31:31 +0530

    master

--- blogger-util/src/main/java/com/blogger/util/Utility.java
+++ blogger-util/src/main/java/com/blogger/util/Utility.java
@@ -3,11 +3,10 @@ package com.blogger.util;
 public class Utility {
 
     public static boolean isNull(String message){
-        if(message==null || message.isEmpty())
+        if(message==null || message.isEmpty()){
             return true;
-//        }
-//            return false;
-        return false;
+        }
+            return false;
     }
 
     public static boolean isNotNull(String message){
